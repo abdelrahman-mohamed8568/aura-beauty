00dc0c17cac3b7cca22bb1571fc19254bed58b75
Abdelrahman <124844787+abdelrahman-mohamed8568@users.noreply.github.com>
2025-04-22 10:05:13 +0200

    SpeedInsights

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import "swiper/css";
-import { montserratFont } from "@/lang/lang";
-import Footer from "@/appComponents/sections/footer";
+import Preloader from "./components/animations/preloader";
 import Navbar from "@/appComponents/sections/navbar";
 import { Provider } from "@/components/ui/provider";
+import Footer from "@/appComponents/sections/footer";
+import { montserratFont } from "@/lang/lang";
 import StoreProvider from "./StoreProvider";
 import ReactLenis from "lenis/react";
-import Preloader from "./components/animations/preloader";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata = {
   title: "Aura Beauty",
   description: "Beauty store advanced products and devices",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
             <Navbar />
             {children}
             <Footer />
+            <SpeedInsights />
           </Provider>
         </StoreProvider>
       </body>
