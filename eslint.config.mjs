ee861945b8d361e00ece24617cbd0d61b1834b9c
Abdelrahman <124844787+abdelrahman-mohamed8568@users.noreply.github.com>
2025-04-22 07:28:09 +0200

    eslintConfig update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-export default [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals"],
+  }),
+];
+
+export default eslintConfig;
